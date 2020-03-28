66244b0ece08694fd4fb1d97f2fc34c647e8e395
roshan1718 <roshanshinde1717@gmail.com>
2020-03-28 15:47:47 +0530

    ADDED code to calculating wages for month

--- EmployeeWageProblem.java
+++ EmployeeWageProblem.java
@@ -4,26 +4,34 @@
 
 public class EmployeeWageProblem{
 
-public static final int IS_FULLTIME=1, IS_PARTTIME=2, WAGE_PER_HOUR=20;
+public static final int IS_FULLTIME=1, IS_PARTTIME=2, NOOFDAYS=20, WAGE_PER_HOUR=20;
 
 	public static void main ( String args[] ){
-		int checkNumber=(int)(Math.floor(Math.random()*10)%3);
-		int salary=0, empHours=0;
-		switch (checkNumber){
-			case IS_FULLTIME:
-				System.out.println("Employee Is Full Time..!");
-				empHours=8;
-				break;
-			case IS_PARTTIME:
-				System.out.println("Employee Is Part Time..!");
-				empHours=4;
-				break;
-			default:
-				System.out.println("Employee Is Absent..");
-				empHours=0;
-				break;
+		int totalSalary=0, salary=0;
+
+		for ( int i=0; i<NOOFDAYS; i++ ){
+			int checkNumber=(int)(Math.floor(Math.random()*10)%3);
+			int empHours=0;
+			switch (checkNumber){
+				case IS_FULLTIME:
+					System.out.println("Employee Is Full Time..!");
+					empHours=8;
+					break;
+				case IS_PARTTIME:
+					System.out.println("Employee Is Part Time..!");
+					empHours=4;
+					break;
+				default:
+					System.out.println("Employee Is Absent..");
+					empHours=0;
+					break;
+			}
+			salary=empHours*WAGE_PER_HOUR;
+			System.out.println("Salary of Employee is.."+salary);
+			totalSalary+=salary;
 		}
-		salary=empHours*WAGE_PER_HOUR;
-		System.out.println("Salary of Employee is.."+salary);
+		System.out.println("ToTal Salary of Employee is.."+ totalSalary);
 	}
+
 }
+
