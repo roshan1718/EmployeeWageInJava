5669a4e0f59ba8a60dea52b874aa8d8082d343d6
roshan1718 <roshanshinde1717@gmail.com>
2020-03-28 16:42:49 +0530

    ADDED code to calculate total salary upto the limit

--- EmployeeWageProblem.java
+++ EmployeeWageProblem.java
@@ -4,14 +4,15 @@
 
 public class EmployeeWageProblem{
 
-public static final int IS_FULLTIME=1, IS_PARTTIME=2, NOOFDAYS=20, WAGE_PER_HOUR=20;
+public static final int IS_FULLTIME=1, IS_PARTTIME=2, MAX_DAYS=20, WAGE_PER_HOUR=20, MAX_HOURS=100 ;
 
 	public static void main ( String args[] ){
-		int totalSalary=0, salary=0;
+		int totalSalary=0, salary=0, totalEmpHours=0, noOfDays=0;
 
-		for ( int i=0; i<NOOFDAYS; i++ ){
+		while ( totalEmpHours < MAX_HOURS && noOfDays < MAX_DAYS ){
 			int checkNumber=(int)(Math.floor(Math.random()*10)%3);
 			int empHours=0;
+			noOfDays+=1;
 			switch (checkNumber){
 				case IS_FULLTIME:
 					System.out.println("Employee Is Full Time..!");
@@ -27,7 +28,8 @@ public static final int IS_FULLTIME=1, IS_PARTTIME=2, NOOFDAYS=20, WAGE_PER_HOUR
 					break;
 			}
 			salary=empHours*WAGE_PER_HOUR;
-			System.out.println("Salary of Employee is.."+salary);
+			totalEmpHours+=empHours;
+			System.out.println("Salary of Employee on "+noOfDays+ "th Day is.."+salary);
 			totalSalary+=salary;
 		}
 		System.out.println("ToTal Salary of Employee is.."+ totalSalary);
