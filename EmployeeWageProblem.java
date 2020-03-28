3c9964e9a568db2345abcae17fb479e76efa9a07
roshan1718 <roshanshinde1717@gmail.com>
2020-03-28 15:12:49 +0530

    ADDED code to part time employee

--- EmployeeWageProblem.java
+++ EmployeeWageProblem.java
@@ -1,19 +1,28 @@
-public class EmployeeWageProblem
-{
-	public static void main ( String args[] ){
+// Description : Employee Wage Problem
+// Author : Roshan Balkrushna Shinde
+// Since 28 March 2020 
 
-		int checkNumber=(int)(Math.floor(Math.random()*10)%2);
-		int salary=0, IS_PRESENT=1,WAGE_PER_HOUR=20,FULL_DAY_HOUR=8;
-		if ( checkNumber == IS_PRESENT )
-		{
-			System.out.println("Employee Is Present..!");
-			salary=(WAGE_PER_HOUR*FULL_DAY_HOUR);
-			System.out.println("Employee Salary = "+salary);
+public class EmployeeWageProblem{
+
+public static final int IS_FULLTIME=1, IS_PARTTIME=2, WAGE_PER_HOUR=20;
+
+	public static void main ( String args[] ){
+		int checkNumber=(int)(Math.floor(Math.random()*10)%3);
+		int salary=0, empHours=0;
+	
+		if ( checkNumber == IS_FULLTIME ){
+			System.out.println("Employee Is Full Time..!");
+			empHours=8;
 		}
-		else
-		{
-			System.out.println("Employee Is Absent..!");
-			System.out.println("Employee Salary = "+salary);
+		else if ( checkNumber == IS_PARTTIME ){
+			System.out.println("Employee Is Part Time..!");
+			empHours=4;
 		}
+		else{
+			System.out.println("Employee Is Absent..");
+			empHours=0;
+		}
+		salary=empHours*WAGE_PER_HOUR;
+		System.out.println("Salary of Employee is.."+salary);
 	}
 }
