ab2f01ec05f3b21db195940b09820699d17dddd8
roshan1718 <roshanshinde1717@gmail.com>
2020-03-28 14:04:37 +0530

    ADDED code to calculate daily employee wage

--- EmployeeWageProblem.java
+++ EmployeeWageProblem.java
@@ -1,12 +1,19 @@
-public class EmployeeWageProblem{	
+public class EmployeeWageProblem
+{
 	public static void main ( String args[] ){
 
 		int checkNumber=(int)(Math.floor(Math.random()*10)%2);
-		int IS_PRESENT=1;
+		int salary=0, IS_PRESENT=1,WAGE_PER_HOUR=20,FULL_DAY_HOUR=8;
 		if ( checkNumber == IS_PRESENT )
+		{
 			System.out.println("Employee Is Present..!");
+			salary=(WAGE_PER_HOUR*FULL_DAY_HOUR);
+			System.out.println("Employee Salary = "+salary);
+		}
 		else
+		{
 			System.out.println("Employee Is Absent..!");
+			System.out.println("Employee Salary = "+salary);
+		}
 	}
 }
-
